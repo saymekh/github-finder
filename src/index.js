42bf29936eb3cb8a9778f46c7d88dc616afcaeb8
Saymek <hsaymek@gmail.com>
2024-05-18 16:17:03 -0500

    package.json

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.js'
-import { BrowserRouter as Router } from 'react-router-dom';
 import './css/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
       <App />
-    </Router>
   </React.StrictMode>,
 )
